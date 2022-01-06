d4983ab5afe978a274d258b7da542ee716be5dc6
calebjenkins <caleb@calebjenkins.com>
2022-01-05 21:14:34 -0600

    update VS Test Nuget packages

--- src/SimpleCalculationsLibTests/EasiestToTestMethodsEvar2_when_adding_should.cs
+++ src/SimpleCalculationsLibTests/EasiestToTestMethodsEvar2_when_adding_should.cs
@@ -1,42 +1,44 @@
 ﻿using System;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Should;
+using FluentAssertions;
+
+
 
 namespace SimpleCalculationsLib.Tests
 {
-	[TestClass()]
-	public class EasiestToTestMethodsEvar2_when_adding_should
-	{
-		/************************
-		 *						*
-		 *   Using Should		*
-		 *						*
-		 ************************/
+    [TestClass()]
+    public class EasiestToTestMethodsEvar2_when_adding_should
+    {
+         /*******************************
+		 *					        	*
+		 *   Using FluentAssertions		*
+		 *						        *
+		 *******************************/
 
-		[TestMethod()]
-		public void Should_combine_two_numbers_to_4()
-		{
-			int expected = 4;
-			var sut = new EasiestToTestMethodsEvar();
-			var result = sut.Add(2, 2);
+        [TestMethod()]
+        public void Should_combine_two_numbers_to_4()
+        {
+            int expected = 4;
+            var sut = new EasiestToTestMethodsEvar();
+            var result = sut.Add(2, 2);
 
-			result.ShouldEqual<int>(expected);
-				  //--------//
-		}
+            result.Should<int>().Be(expected);
+            //--------//
+        }
 
-		[TestMethod()]
-		public void Should_combine_two_numbers_Different_Numbers()
-		{
-			int expected = 7;
-			var sut = new EasiestToTestMethodsEvar();
-			var result = sut.Add(3, 4);
+        [TestMethod()]
+        public void Should_combine_two_numbers_Different_Numbers()
+        {
+            int expected = 7;
+            var sut = new EasiestToTestMethodsEvar();
+            var result = sut.Add(3, 4);
 
-			result.ShouldBeGreaterThan<int>(4);
-			result.ShouldBeLessThanOrEqualTo<int>(10);
+            result.Should<int>().BeGreaterThan(4);
+            result.Should<int>().BeLessThanOrEqualTo(10);
 
-			result.ShouldEqual<int>(expected);
-		}
+            result.Should<int>().Be(expected);
+        }
 
-	}
+    }
 }
