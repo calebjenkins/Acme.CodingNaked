6cdbf2bc863833b6d9b66aa700cfa10c29ed4ee9
calebjenkins <caleb@calebjenkins.com>
2022-01-05 22:04:05 -0600

    update path for workflow

--- src/SlightlyHarderLibTests/FavoriteColorGenerator2_When_Getting_withFakes_.cs
+++ src/SlightlyHarderLibTests/FavoriteColorGenerator2_When_Getting_withFakes_.cs
@@ -1,50 +1,50 @@
-﻿using System;
-using System.Linq;
-using Microsoft.QualityTools.Testing.Fakes;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿//using System;
+//using System.Linq;
+//using Microsoft.QualityTools.Testing.Fakes;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 
-namespace SlightlyHarderLib.Tests
-{
-	public class FavoriteColorGenerator2_When_Getting_withFakes_
-	{
+//namespace SlightlyHarderLib.Tests
+//{
+//	public class FavoriteColorGenerator2_When_Getting_withFakes_
+//	{
 
-	/****************************************
-	 *										*
-	 *            DONT USE FAKES			*
-	 *										*
-	 * *************************************/
+//	/****************************************
+//	 *										*
+//	 *            DONT USE FAKES			*
+//	 *										*
+//	 * *************************************/
 
-		// [TestMethod()]
-		public void On_Tuesday_Should_Be_Yellow()
-		{
-			using (ShimsContext.Create())
-			{
-				//System.Fakes.ShimDateTime.NowGet = () =>
-				//{
-				//	return new DateTime(2017, 1, 31); // This was a Tuesday
-				//};
+//		// [TestMethod()]
+//		public void On_Tuesday_Should_Be_Yellow()
+//		{
+//			using (ShimsContext.Create())
+//			{
+//				//System.Fakes.ShimDateTime.NowGet = () =>
+//				//{
+//				//	return new DateTime(2017, 1, 31); // This was a Tuesday
+//				//};
 
-				string expected = "Yellow";
-				var colors = new FavoriteColorGenerator();
+//				string expected = "Yellow";
+//				var colors = new FavoriteColorGenerator();
 
-				var result = colors.GetFavorite();
+//				var result = colors.GetFavorite();
 
-				Assert.AreEqual(expected, result);
+//				Assert.AreEqual(expected, result);
 
-			}
-		}
+//			}
+//		}
 
 
-		// [TestMethod]
-		public void On_Wed_Should_Be_Blue()
-		{
-			string expected = "Blue";
-			var colors = new FavoriteColorGenerator();
+//		// [TestMethod]
+//		public void On_Wed_Should_Be_Blue()
+//		{
+//			string expected = "Blue";
+//			var colors = new FavoriteColorGenerator();
 
-			var result = colors.GetFavorite();
+//			var result = colors.GetFavorite();
 
-			Assert.AreEqual(expected, result);
-		}
-	}
-}
+//			Assert.AreEqual(expected, result);
+//		}
+//	}
+//}
