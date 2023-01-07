a82e740077c706c18cedd8fe6e05951abc5d808c
Caleb Jenkins <caleb@calebjenkins.com>
2023-01-06 21:32:04 -0600

    updating BDD tests to always specific day - otherwise flakey on CI serve

--- src/BDD/BDD_Tests/Given_AccountService_/When_CalculateTripPriceForAccount_SilverStatus_.cs
+++ src/BDD/BDD_Tests/Given_AccountService_/When_CalculateTripPriceForAccount_SilverStatus_.cs
@@ -2,7 +2,6 @@
 
 public class When_CalculateTripPriceForAccount_SilverStatus_
 {
-    private AccountService _serv;
     IDateTimeProvider _dtProvider;
     Func<DateTime> _funcDt = () => DateTime.Now;
 
@@ -10,16 +9,17 @@ public class When_CalculateTripPriceForAccount_SilverStatus_
     public When_CalculateTripPriceForAccount_SilverStatus_()
     {
         _dtProvider = new DateTimeProvider();
-        _serv = new AccountService(_dtProvider, _funcDt);
     }
 
     [Fact]
     public void with_NoWeekendDiscount_Should_return_BaseSilverAmount()
     {
         // No Weekend Discount // Silver Status * 500 points
+        var serv = new AccountService(_dtProvider, () => TestCommon.Wednesday);
         var acct = TestCommon.SilverAccount;
         var trip = TestCommon.TripToBuy;
-        var results = _serv.CalculateTripPriceForAccount(acct, trip);
+
+        var results = serv.CalculateTripPriceForAccount(acct, trip);
         results.Should().Be(Ref.SilverPricePerPoint * trip.Points);
     }
 
@@ -27,12 +27,12 @@ public class When_CalculateTripPriceForAccount_SilverStatus_
     public void with_WeekendDiscount_Should_return_BaseSilverAmount_Less_DiscountAmount()
     { 
         // Weekend Discount - Set Account Service to Weekend for testing
-        _serv = new AccountService(_dtProvider, () => TestCommon.Sunday);
+        var serv = new AccountService(_dtProvider, () => TestCommon.Sunday);
 
         var acct = TestCommon.SilverAccount;
         var trip = TestCommon.TripToBuy;
 
-        var results = _serv.CalculateTripPriceForAccount(acct, trip);
+        var results = serv.CalculateTripPriceForAccount(acct, trip);
         results.Should().Be(Ref.SilverPricePerPoint * trip.Points - Ref.WeekendDiscountAmount);
     }
 }
--- src/BDD/BDD_Tests/Given_AccountService_/When_Calculating_Trips_.cs
+++ src/BDD/BDD_Tests/Given_AccountService_/When_Calculating_Trips_.cs
@@ -4,24 +4,23 @@ namespace BDD_Tests.Given_AccountService_;
 
 public class When_CalculateTripPriceForAccount_StandardStatus_
 {
-    private AccountService _serv;
     IDateTimeProvider _dtProvider;
-    Func<DateTime> _funcDt = () => DateTime.Now;
+    Func<DateTime> _funcDt = () => TestCommon.Wednesday;
 
     // When
     public When_CalculateTripPriceForAccount_StandardStatus_()
     {
         _dtProvider = new DateTimeProvider();
-        _serv = new AccountService(_dtProvider, _funcDt);
     }
 
     [Fact]
     public void with_NoDiscount_Should_return_BaseAmount()
     {
         // No Discount // No Status * 500 points
+        var serv = new AccountService(_dtProvider, () => TestCommon.Wednesday);
         var acct = TestCommon.StandardAccount;
         var trip = TestCommon.TripToBuy;
-        var results = _serv.CalculateTripPriceForAccount(acct, trip);
+        var results = serv.CalculateTripPriceForAccount(acct, trip);
         results.Should().Be(TestCommon.BasePrice);
     }
 
@@ -29,11 +28,11 @@ public class When_CalculateTripPriceForAccount_StandardStatus_
     public void with_WeekendDiscount_Should_return_BaseAmount_Less_DiscountAmount()
     {
         // Weekend Discount // No Status * 500 points
-        _serv = new AccountService(_dtProvider, ()=> TestCommon.Sunday);
+        var serv = new AccountService(_dtProvider, ()=> TestCommon.Sunday);
         var acct = TestCommon.StandardAccount;
         var trip = TestCommon.TripToBuy;
 
-        var results = _serv.CalculateTripPriceForAccount(acct, trip);
+        var results = serv.CalculateTripPriceForAccount(acct, trip);
         results.Should().Be(TestCommon.BasePrice - Ref.WeekendDiscountAmount);
     }
 }
