4931e7136eb23025808a27cb600b508bb6aba5ab
Caleb Jenkins <caleb@calebjenkins.com>
2023-01-08 12:08:15 -0600

    refactor moq logging extension

--- src/WithExtensionsLibTests/BusinessProcessStartTests_Reset_Failure.cs
+++ src/WithExtensionsLibTests/BusinessProcessStartTests_Reset_Failure.cs
@@ -39,6 +39,7 @@ namespace WithExtensionsLib.Tests
             var loggerMock = new Mock<ILogger>();
             _logger = loggerMock.Object;
 
+
             var sut = new BusinessProcessStart(bt, _logger);
 
             // Act
@@ -46,47 +47,8 @@ namespace WithExtensionsLib.Tests
 
             // Assert
             btMock.Verify();
-            loggerMock.VerifyLogging(expecteLogText, LogLevel.Error, Times.Once());
+            loggerMock.VerifyLogging("Reset Failed", LogLevel.Error, Times.Once());
 
-        }
-    }
-
-    public static class MoqLoggingExtensions
-    {
-        // Hat Tip Adam Storr
-        // Awesome Extension Method for Verifying via https://adamstorr.azurewebsites.net/blog/mocking-ilogger-with-moq
-        // https://github.com/WestDiscGolf/Random
-        public static Mock<ILogger> VerifyLogging(this Mock<ILogger> logger, string expectedMessage, LogLevel expectedLogLevel = LogLevel.Debug, Times? times = null)
-        {
-            times ??= Times.Once();
-
-            Func<object, Type, bool> state = (v, t) => v.ToString().CompareTo(expectedMessage) == 0;
-
-            logger.Verify(
-                x => x.Log(
-                    It.Is<LogLevel>(l => l == expectedLogLevel),
-                    It.IsAny<EventId>(),
-                    It.Is<It.IsAnyType>((v, t) => state(v, t)),
-                    It.IsAny<Exception>(),
-                    It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)), (Times)times);
-
-            return logger;
-        }
-        public static Mock<ILogger<T>> VerifyLogging<T>(this Mock<ILogger<T>> logger, string expectedMessage, LogLevel expectedLogLevel = LogLevel.Debug, Times? times = null)
-        {
-            times ??= Times.Once();
-
-            Func<object, Type, bool> state = (v, t) => v.ToString().CompareTo(expectedMessage) == 0;
-
-            logger.Verify(
-                x => x.Log(
-                    It.Is<LogLevel>(l => l == expectedLogLevel),
-                    It.IsAny<EventId>(),
-                    It.Is<It.IsAnyType>((v, t) => state(v, t)),
-                    It.IsAny<Exception>(),
-                    It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)), (Times)times);
-
-            return logger;
         }
     }
 }
---
+++ src/WithExtensionsLibTests/MoqLoggingExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace WithExtensionsLib.Tests
+{
+    public static class MoqLoggingExtensions
+    {
+        // Hat Tip Adam Storr
+        // Awesome Extension Method for Verifying via https://adamstorr.azurewebsites.net/blog/mocking-ilogger-with-moq
+        // https://github.com/WestDiscGolf/Random
+        public static Mock<ILogger> VerifyLogging(this Mock<ILogger> logger, string expectedMessage, LogLevel expectedLogLevel = LogLevel.Debug, Times? times = null)
+        {
+            times ??= Times.Once();
+
+            Func<object, Type, bool> state = (v, t) => v.ToString().CompareTo(expectedMessage) == 0;
+
+            logger.Verify(
+                x => x.Log(
+                    It.Is<LogLevel>(l => l == expectedLogLevel),
+                    It.IsAny<EventId>(),
+                    It.Is<It.IsAnyType>((v, t) => state(v, t)),
+                    It.IsAny<Exception>(),
+                    It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)), (Times)times);
+
+            return logger;
+        }
+        public static Mock<ILogger<T>> VerifyLogging<T>(this Mock<ILogger<T>> logger, string expectedMessage, LogLevel expectedLogLevel = LogLevel.Debug, Times? times = null)
+        {
+            times ??= Times.Once();
+
+            Func<object, Type, bool> state = (v, t) => v.ToString().CompareTo(expectedMessage) == 0;
+
+            logger.Verify(
+                x => x.Log(
+                    It.Is<LogLevel>(l => l == expectedLogLevel),
+                    It.IsAny<EventId>(),
+                    It.Is<It.IsAnyType>((v, t) => state(v, t)),
+                    It.IsAny<Exception>(),
+                    It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)), (Times)times);
+
+            return logger;
+        }
+    }
+}
